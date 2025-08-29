dabe4d6aaabc2614be2d4a81c22e13bd972b98eb
Eilidh Morrice-Ashdown <eilidhma@gmail.com>
2025-08-28 19:29:50 -0700

    node update

--- comps/Logo/index.js
+++ comps/Logo/index.js
@@ -5,54 +5,56 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const Cont = styled.div`
-    display: flex;
-    position: fixed;
-    top:${({top})=>top};
-    transition: top 0.25s ease;
-    left:0;
-    z-index: 200;
-    width: 150px;
-    @media (max-width: 900px) {
-        width: 140px;
-    }
-    @media (max-width: 700px) {
-        width: 100px;
-        display: none;
-    }
-`
+  display: flex;
+  position: fixed;
+  top: ${({ top }) => top};
+  transition: top 0.25s ease;
+  left: 0;
+  z-index: 200;
+  width: 150px;
+  @media (max-width: 900px) {
+    width: 140px;
+  }
+  @media (max-width: 700px) {
+    width: 100px;
+    display: none;
+  }
+`;
 
-export default function Logo(){
+export default function Logo() {
+  const r = useRouter();
+  const [scrolling, setScrolling] = useState(false);
+  const [prevScrollY, setPrevScrollY] = useState(0);
 
-    const r = useRouter();
-    const [scrolling, setScrolling] = useState(false);
-    const [prevScrollY, setPrevScrollY] = useState(0);
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        const currentScrollY = window.scrollY;
-  
-        setScrolling(currentScrollY > 0 && currentScrollY > prevScrollY);
-        setPrevScrollY(currentScrollY);
-      };
-  
-      window.addEventListener("scroll", handleScroll);
-  
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }, [prevScrollY]);
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
 
-    console.log(scrolling)
+      setScrolling(currentScrollY > 0 && currentScrollY > prevScrollY);
+      setPrevScrollY(currentScrollY);
+    };
 
-    return (
-        <motion.div
-        initial={{opacity:0}}
-        animate={{opacity:1}}
-        transition={{delay:3, duration:2}}
-        >
-            <Cont top={scrolling ? "-150px" : "0"}>
-                <img onClick={()=>r.push('/')} src="gold_logo.svg" style={{padding:10, margin:15, cursor: "pointer"}} width={'100%'}/>
-            </Cont>
-        </motion.div>
-    )
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollY]);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 3, duration: 2 }}
+    >
+      <Cont top={scrolling ? "-150px" : "0"}>
+        <img
+          onClick={() => r.push("/")}
+          src="gold_logo.svg"
+          style={{ padding: 10, margin: 15, cursor: "pointer" }}
+          width={"100%"}
+        />
+      </Cont>
+    </motion.div>
+  );
 }
